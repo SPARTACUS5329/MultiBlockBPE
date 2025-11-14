d16b1c20cd1f032bcdf32afd889f15b926031f57
Aditya Kovilur <aditya@Adityas-MacBook-Pro.local>
2025-11-13 20:49:17 -0600

    using flex to lex the file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <string>
 
+extern "C" {
+int yylex();
+extern char *yytext;
+extern FILE *yyin;
+enum TokenType { TOKEN = 1 };
+}
+
 void error(const char *msg) {
   perror(msg);
   exit(1);
@@ -13,15 +20,27 @@ int main(int argc, char *argv[]) {
     error("[main] Not enough input arguments: expected file address");
   }
 
-  const std::string filename = argv[1];
-  std::string content = readFile(filename);
-  DLLToken *dllHead = initDLL(content);
-
-  auto *dllToken = dllHead;
-  while (dllToken) {
-    std::cout << dllToken->token.val << " ";
-    dllToken = dllToken->next;
+  const char *filename = argv[1];
+  FILE *f = fopen(filename, "r");
+  if (!f) {
+    error("[main] File not found");
   }
+
+  yyin = f;
+
+  int token;
+  while ((token = yylex()) != 0) {
+    switch (token) {
+    case TOKEN:
+      printf("TOKEN: %s\n", yytext);
+      break;
+
+    default:
+      std::cout << "[UNKNOWN] " << yytext << "\n";
+    }
+  }
+
+  fclose(f);
 
   return 0;
 }
