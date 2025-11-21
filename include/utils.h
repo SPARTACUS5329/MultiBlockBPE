14535686cd57bc9ed849176e41b96d57483f5da1
Aditya Kovilur <kovilur@login28.chn.perlmutter.nersc.gov>
2025-11-21 13:30:44 -0800

    removed DLL

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "DLLToken.h"
 #include <string>
 
 std::string readFile(const std::string &path);
-DLLToken *initDLL(const std::string &s);
--- src/Token.cpp
+++ src/Token.cpp
@@ -1,16 +1,4 @@
 #include "Token.h"
-#include "DLLToken.h"
 #include <iostream>
 
 Token::Token(int id, const std::string &val) : id(id), val(val) {}
-
-void Token::removeOccurrences(const std::vector<DLLToken *> &removables) {
-  for (auto *r : removables) {
-    auto it = occurrences.find(r);
-    if (it != occurrences.end()) {
-      occurrences.erase(it);
-      std::cout << "Removed occurrence of token " << id << " from DLLToken "
-                << r->id << "\n";
-    }
-  }
-}
