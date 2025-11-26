daa6f1a32867755ae385a376e5757d9b3a0b0a43
Varun <vcs@tamu.edu>
2025-11-25 23:01:12 -0600

    Add OpenMP task parallelization and updates

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <omp.h>
+#include <mutex>
 
 #define MAX_SEQ_LEN 1024
 
+std::mutex vecMutex; // Mutex for synchronizing access to shared vectors
+ 
 extern "C"
 {
   int yylex();
@@ -44,29 +48,42 @@ int main(int argc, char *argv[])
   std::vector<int> tokens;
   std::vector<int> nextToken;
   std::unordered_map<std::string, int> tokenIDMap;
-  // populate token_id_map from vocab.json
-  // token_id_map["\'"] = 12;
-  // token_id_map["s"] = 24;
 
-  while ((token = yylex()) != 0)
+  #pragma omp parallel
+  #pragma omp single  // Single thread to call yylex
   {
-    switch (token)
+    while ((token = yylex()) != 0)
     {
-    case PRE_TOKEN:
-      // Parallelize the for loop
-      int i;
-      for (i = 0; yytext[i] != '\0'; i++)
-      {
-        std::string key(1, yytext[i]);
-        tokens.push_back(tokenIDMap[key]);
-        nextToken.push_back(i + 1);
-      }
-      nextToken.back() = -1;
-      break;
-    default:
-      std::cout << "[UNKNOWN] " << yytext << "\n";
+        if (token == PRE_TOKEN)
+        {
+            // IMPORTANT: copy yytext immediately
+            std::string textCopy = yytext;
+
+            #pragma omp task firstprivate(textCopy)
+            {   
+                printf("Thread %d handling chunk: %s\n", omp_get_thread_num(), textCopy.c_str());
+                std::vector<int> localTokens;
+                std::vector<int> localNext;
+
+                for (int i = 0; i < textCopy.size(); i++) {
+                    std::string key(1, textCopy[i]);
+                    localTokens.push_back(tokenIDMap[key]);
+                    localNext.push_back(i + 1);
+                }
+                localNext.back() = -1;
+
+                // append to the global vectors safely
+                std::lock_guard<std::mutex> lock(vecMutex);
+                tokens.insert(tokens.end(), localTokens.begin(), localTokens.end());
+                nextToken.insert(nextToken.end(), localNext.begin(), localNext.end());
+            }
+        }
     }
-  }
+
+    // Wait until ALL tasks complete
+    #pragma omp taskwait
+}
+
 
   fclose(f);
 
